b60ab3586295c1fbbd4de8e7758df2883a47bd69
Isabela Pastorini <isabela.pastorini643@gmail.com>
2020-10-05 20:41:59 -0300

    Second try

---
+++ Practical_Class/my-app/src/test/java/com/mycompany/app/DollarTest.java
@@ -0,0 +1,21 @@
+package com.mycompany.app;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class DollarTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void testMultiplication()
+    {
+        Dollar five = new Dollar(5);
+        five.times(2);
+        assertEquals(10,five.amount);
+    }
+}
